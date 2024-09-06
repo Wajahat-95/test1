1a5020a24aa109ff0cd506cb4adfc18fea3f8901
Muhammad Wajahat <wajahat.siddiqui111@gmail.com>
2024-09-06 18:16:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 const express = require("express");
-const bodyParser = require('body-parser');
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config()
 const port = process.env.PORT || 9000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("<h1>Hi This is Wajahat</h1><br><h2>Server is Running!</h2>");
 });
 
-app.post("/sum", (req,res) => {
-    const numbers = req.body;
+app.post('/calculate', (req, res) => {
+    const numbers = req.body.numbers;
 
-    if(!Array.isArray(numbers)) {
-        return res.status(400).json({error: "input must be an array"})
+    if (!Array.isArray(numbers)) {
+        return res.status(400).send('Input should be an array of int32 numbers');
     }
 
-    try {
-        const sum = numbers.reduce((acc, curr) => {
-            if (!Number.isInteger(curr) || curr < -2147483648 || curr > 2147483647) {
-                throw new Error("All elements must be 32-bit integers");
-            }
-            return acc + curr;
-        }, 0);
+    const result = numbers.reduce((acc, num) => acc + num, 0);
 
-        res.json({ result: sum });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-})
-
+    res.json({ result });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
